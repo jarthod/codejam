d0415cd52c5867f5d35a0a848aaa7b3f95802584
Adrien Jarthon <jobs@adrienjarthon.com>
2014-04-21 13:52:50 +0200

    minesweeper draft

--- 2014/qualification/3-minesweeper_master/minesweeper_master.cpp
+++ 2014/qualification/3-minesweeper_master/minesweeper_master.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
+
+class Board
+{
+public:
+  Board(int rows, int cols)
+  {
+    this->rows = rows;
+    this->cols = cols;
+    for (int r = 0; r < rows; ++r)
+    {
+      memset (cells[r],'*',cols);
+      cells[r][cols] = '\0';
+    }
+    cells[0][0] = 'c';
+  };
+
+  int   size() const
+  {
+    return rows * cols;
+  }
+
+  void  print() const
+  {
+    for (int r = 0; r < rows; ++r)
+      cout << cells[r] << endl;
+  };
+
+  int rows, cols;
+  char cells[50][50];
+};
+
+int solve(Board board&, int mines)
+{
+
+}
 
 int main(int argc, char const *argv[])
 {
@@ -12,7 +48,16 @@ int main(int argc, char const *argv[])
     file.open(argv[1]);
     file >> cases;
     for (int c = 0; c < cases; c++) {
+      int rows, cols, mines;
+      file >> rows >> cols >> mines;
+      Board board(rows, cols);
       cout << "Case #" << c+1 << ":" << endl;
+      if (board.size() - mines == 1)
+        board.print();
+      else if (board.size() - mines == 0)
+        cout << "Impossible" << endl;
+      else
+        solve(board, mines);
     }
     file.close();
     return 0;
